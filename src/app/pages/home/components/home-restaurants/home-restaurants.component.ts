9e56b3e279c5211636a9150c58b3b99e97bc643c
r_ur10us <bondmisha6@gmail.com>
2023-12-19 19:16:33 +0200

    name

---
+++ src/app/pages/home/components/home-restaurants/home-restaurants.component.ts
@@ -0,0 +1,89 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-restaurants',
+  templateUrl: './home-restaurants.component.html',
+  styleUrls: ['./home-restaurants.component.scss']
+})
+export class HomeRestaurantsComponent {
+  public restaurants = [
+    {
+      title: 'Royal Sushi House',
+      time: '30-40 min',
+      img: 'assets/royal_sushi.png',
+      price: '$32 min sum',
+      cart: '5',
+      tags: [
+        {
+          title: 'Sushi',
+          img: 'assets/sushi.png'
+        }
+      ]
+    },
+    {
+      title: 'Royal Sushi House',
+      time: '30-40 min',
+      img: 'assets/royal_sushi.png',
+      price: '$32 min sum',
+      cart: '5',
+      tags: [
+        {
+          title: 'Sushi',
+          img: 'assets/sushi.png'
+        }
+      ]
+    },
+    {
+      title: 'Royal Sushi House',
+      time: '30-40 min',
+      img: 'assets/royal_sushi.png',
+      price: '$32 min sum',
+      cart: '5',
+      tags: [
+        {
+          title: 'Sushi',
+          img: 'assets/sushi.png'
+        }
+      ]
+    },
+    {
+      title: 'Royal Sushi House',
+      time: '30-40 min',
+      img: 'assets/royal_sushi.png',
+      price: '$32 min sum',
+      cart: '5',
+      tags: [
+        {
+          title: 'Sushi',
+          img: 'assets/sushi.png'
+        }
+      ]
+    },
+    {
+      title: 'Royal Sushi House',
+      time: '30-40 min',
+      img: 'assets/royal_sushi.png',
+      price: '$32 min sum',
+      cart: '5',
+      tags: [
+        {
+          title: 'Sushi',
+          img: 'assets/sushi.png'
+        }
+      ]
+    },
+    {
+      title: 'Royal Sushi House',
+      time: '30-40 min',
+      img: 'assets/royal_sushi.png',
+      price: '$32 min sum',
+      cart: '5',
+      tags: [
+        {
+          title: 'Sushi',
+          img: 'assets/sushi.png'
+        }
+      ]
+    },
+  ]
+}
